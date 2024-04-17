700bc65a689a05dcf84d72b66667c6177ac3df8b
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-04-17 18:15:58 -0500

    fixed

--- Part1/part1.m
+++
@@ -1,9 +0,0 @@
-% Read in the data
-x = csvread('x.csv');
-y = csvread('y.csv');
-
-% Make predictions using the neural network function
-predicted_z = neuralNetworkFunction([x ; y]);
-
-% Save the predicted values to a CSV file
-csvwrite('z-predicted.csv', predicted_z);
